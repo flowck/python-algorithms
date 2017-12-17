2432b7a4304b72861c6a5546da9aa2fe3339a83f
Firmino Changani <flowck96@gmail.com>
2017-12-17 13:04:14 +0100

    Rename exercise folder

--- sapce-distance/space-distance.py
+++
@@ -1,5 +0,0 @@
-# Algorithms:
-
-# Build a program that read a spacial cordinates of two points: P1(x1, y1) and
-# P2(x2, y2). Then calculate the distance between of two points from this formule: 
-# d = √(x2-x1)^2 + (y2-y1)^2 
