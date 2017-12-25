5b76fc071a331c198637b004ed988fa0fc7b3a6f
Firmino Changani <flowck96@gmail.com>
2017-12-25 07:13:02 +0100

    Comment the function body following PEP 257

--- fibonacci-sequence/fibonacci.py
+++ fibonacci-sequence/fibonacci.py
@@ -2,7 +2,8 @@
 
 # I found the solution on a book called Beginning Python.
 
-def fibonacci(n): 
+def fibonacci(n):
+	""" fibonacci(n) takes a int as an argument, and return the fibonacci sequence """
 	fibs = [0, 1]
 	for i in range(n):
 		fibs.append(fibs[-2] + fibs[-1])
